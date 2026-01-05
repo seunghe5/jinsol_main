8b14991695495b3ef6a3daa695641f44126df919
seunghe5 <seunghe5@gmail.com>
2026-01-05 14:53:30 +0900

    Fix: adjust internal library paths in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   decode: require('jsonwebtoken/decode'),
   verify: require('./verify'),
   sign: require('jsonwebtoken/sign'),
-  JsonWebTokenError: require('./lib/JsonWebTokenError'),
-  NotBeforeError: require('./lib/NotBeforeError'),
-  TokenExpiredError: require('./lib/TokenExpiredError'),
+  JsonWebTokenError: require('jsonwebtoken/lib/JsonWebTokenError'),
+  NotBeforeError: require('jsonwebtoken/lib/NotBeforeError'),
+  TokenExpiredError: require('jsonwebtoken/lib/TokenExpiredError'),
 };
